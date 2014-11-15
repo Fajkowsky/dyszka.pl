1cec8891c0b86b950181b4cfdee4a0bf9d4352f2
Aneta <aneta.harnasz@gmail.com>
2014-11-15 22:21:25 +0100

    [001] routing basic config

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,1 +1,30 @@
-angular.module("app", []);
+var myApp = angular.module("app", ["ngRoute"]);
+
+myApp.config(function ($routeProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl: "templates/home.html",
+            controller: "mainController"
+        })
+        .when("/login", {
+            templateUrl: "templates/login.html",
+            controller: "loginController"
+        })
+        .
+        when("/register", {
+            templateUrl: "templates/register.html",
+            controller: "registerController"
+        });
+});
+
+myApp.controller('mainController', function ($scope) {
+    $scope.message = 'Everyone come and see how good I look!';
+});
+
+myApp.controller('loginController', function ($scope) {
+    $scope.message = 'Look! I am an login page.';
+});
+
+myApp.controller('registerController', function ($scope) {
+    $scope.message = 'Contact us! JK. This is just a demo.';
+});
