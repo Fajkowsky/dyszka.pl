f4e70adbc340874f0e890762f22ba1d6740363dd
Aneta <aneta.harnasz@gmail.com>
2014-12-02 16:51:12 +0100

    [001] - add controller, screen, and post for registration * fix data-binding

--- frontend/js/services/AjaxService.js
+++ frontend/js/services/AjaxService.js
@@ -1,14 +1,15 @@
-angular.module("myApp").service("AjaxService", function ($http) {
-    var url = "http://127.0.0.1:8000/";
+angular.module("myApp").service("AjaxService", function ($http, $location) {
+    var url = "http://127.0.0.1:8000";
 
     this.postTest = function (data) {
-        return $http.post(url + "/user", data);
+        return $http.post(url + "/user/", data);
     };
 
     this.postRegister = function (data) {
-        return $http.post(url + "/register", data).success(
+        return $http.post(url + "/register/", data).success(
             function (data) {
                 console.log(data);
+                $location.goToUrl("/");
             });
     };
 });
