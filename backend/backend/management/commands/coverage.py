4ffefc3135115ee500b3e7aadbd37b7d4cc77134
Fajkowsky <dawid.fajkowski@skygate.pl>
2014-12-20 15:56:06 +0100

    added coverage to manage.py

--- backend/backend/management/commands/coverage.py
+++ backend/backend/management/commands/coverage.py
@@ -1,8 +1,11 @@
 from django.core.management.base import BaseCommand
+from subprocess import call
 
 
 class Command(BaseCommand):
     help = 'Make coverage report for tests.'
 
     def handle(self, *args, **options):
-        pass
+        call(["coverage", "run", "manage.py", "test"])
+        call(["coverage", "html"])
+        call(["chromium-browser", "htmlcov/index.html"])
