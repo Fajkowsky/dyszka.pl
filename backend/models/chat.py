31bf29774ad965d1d5792df6878051719b3c1baa
Fajkowsky <dawidfajkowski@gmail.com>
2015-05-14 18:37:08 +0200

    added chat model

---
+++ backend/models/chat.py
@@ -0,0 +1,8 @@
+from . import db
+
+
+class Chat(db.Model):
+    __tablename__ = 'chat'
+    id = db.Column(db.Integer, primary_key=True)
+    first_user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    second_user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
