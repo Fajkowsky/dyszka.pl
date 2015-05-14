2dff088901af1a1c9f542e4cd191d4ac4a22e6dc
Fajkowsky <dawidfajkowski@gmail.com>
2015-05-14 18:41:05 +0200

    added message model

---
+++ backend/models/message.py
@@ -0,0 +1,11 @@
+from . import db
+from datetime import datetime
+
+
+class Message(db.Model):
+    __tablename__ = 'message'
+    id = db.Column(db.Integer, primary_key=True)
+    chat_id = db.Column(db.Integer, db.ForeignKey('chat.id'))
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    create_time = db.Column(db.DateTime, default=datetime.now)
+    message = db.Column(db.Text)
