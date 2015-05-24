5189334ce16b725987b7cd5bf693423a7ef89ba4
Fajkowsky <dawidfajkowski@gmail.com>
2015-05-24 19:48:40 +0200

    fixed db creation

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,4 +1,4 @@
 import os
-BASE_DIR = os.path.realpath(os.path.join('..', os.path.dirname(__file__)))
+BASE_DIR = os.path.realpath(os.path.dirname(__file__) + '/..')
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}/develop.db'.format(BASE_DIR)
 DEBUG = True
